b609d79f7c266a8702c0236c70c98d58faf5fcb7
http://jayferd.us/ <jjmadkisson@gmail.com>
2014-04-05 00:47:19 -0700

    simplify help a bit and consolidate logging

--- src/goodbot/bot.clj
+++ src/goodbot/bot.clj
@@ -4,10 +4,7 @@
    [irclj.core :as irclj]
    [goodbot.parse :refer [extract-command extract-word]]])
 
-(defn select-plugin [plugins name]
-  (->> plugins (filter #(= (:command %) name)) first))
-
-(defn make-help [plugins]
+(defn add-help [plugins]
   (declare plugins-with-help)
   (defn help-handler [irc message]
     (defn handle [command]
@@ -24,21 +21,22 @@
      :doc ".help <command> : print documentation for the given command"
      :handler help-handler})
   (def plugins-with-help (conj plugins help-plugin))
-  help-plugin)
+  plugins-with-help)
 
 (defn respond-with [irc message responses]
   (when-not (nil? responses)
     (def vec-responses (if (coll? responses) responses [responses]))
-    (doseq [r vec-responses] (irclj/reply irc message r))))
+    (doseq [r vec-responses]
+      (println "RESPONDING:" r)
+      (irclj/reply irc message r))))
 
 (defn make-callback [plugins]
-  (def plugins-with-help (conj plugins (make-help plugins)))
+  (def plugins-with-help (add-help plugins))
   (fn [irc message]
     (when-let [[command rest-of-text] (extract-command message)]
-      (println "COMMAND: " command)
+      (println "COMMAND: " command rest-of-text)
       (when-let [plugin (select-plugin plugins-with-help command)]
         (def updated-message (assoc message :text rest-of-text) )
         (def handler (:handler plugin))
         (when-let [responses (handler irc updated-message)]
-          (println "RESPONSES: " responses)
           (respond-with irc message responses))))))
