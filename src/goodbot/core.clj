2a47d5a62dece285125736dcbddcb19f5ea47a14
http://jayferd.us/ <jjmadkisson@gmail.com>
2014-04-07 14:14:10 -0700

    provide environment variables for host, port, nick, channel

--- src/goodbot/core.clj
+++ src/goodbot/core.clj
@@ -5,11 +5,17 @@
             [goodbot.plugins.ping :as ping]
             [goodbot.plugins.clojure :as clj]))
 
+(def host    (or (System/getenv "GOODBOT_HOST")    "irc.freenode.net"))
+(def port    (or (System/getenv "GOODBOT_PORT")    6667))
+(def nick    (or (System/getenv "GOODBOT_NICK")    "goodbot-test"))
+(def channel (or (System/getenv "GOODBOT_CHANNEL") "#goodbot-test"))
+
 (defn -main
   "Starts the bot"
   [& args]
-  (let [bot (irclj/connect "irc.freenode.net" 6667 "goodbot-test"
+  (println (str "connecting to " host ":" port channel " as " nick))
+  (let [bot (irclj/connect host port nick
                            :callbacks {:privmsg (bot/make-callback
                                                   [ping/plugin
                                                    clj/plugin])})]
-    (irclj/join bot "#csuatest")))
+    (irclj/join bot channel)))
