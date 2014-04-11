260485b0b426936daa5db56a75dc1ef495d43aa0
http://jayferd.us/ <jjmadkisson@gmail.com>
2014-04-11 15:22:30 -0700

    .who: forbid overriding users' self-definitions

--- src/goodbot/plugins/who.clj
+++ src/goodbot/plugins/who.clj
@@ -6,24 +6,29 @@
 
 (def who-is-query '[:find ?e :in $ ?nick :where [?e :who.entry/nick ?nick]])
 
-(defmulti handle-who
-  (fn [_ _ c _] c))
-
-(defmethod handle-who "add" [irc message _ rest]
-  (def conn (db/get-conn irc))
-  (when-let [[who-nick desc] (extract-word {:text rest})]
-    (println "add-who" message)
-    @(datomic/transact conn [{:db/id (datomic/tempid :db.part/who)
-                              :who.entry/nick who-nick
-                              :who.entry/submitter (:nick message)
-                              :who.entry/desc desc}])
-    (str "added " who-nick " as " desc " (by " (:nick message) ")")))
-
 (defn find-entry [db nick]
   (when-let [[id] (->> db
                        (datomic/q who-is-query db nick)
                        first)]
     (datomic/entity db id)))
+
+(defmulti handle-who
+  (fn [_ _ c _] c))
+
+(defmethod handle-who "add" [irc message _ rest]
+  (def conn (db/get-conn irc))
+  (def db (datomic/db conn))
+  (when-let [[who-nick desc] (extract-word {:text rest})]
+    (def entry (find-entry db who-nick))
+    (if (and entry
+             (= (:who.entry/submitter entry) who-nick)
+             (not= who-nick (:nick message)))
+      "cannot override a user's self-definition"
+      (do @(datomic/transact conn [{:db/id (datomic/tempid :db.part/who)
+                                    :who.entry/nick who-nick
+                                    :who.entry/submitter (:nick message)
+                                    :who.entry/desc desc}])
+          (str "added " who-nick " as " desc " (by " (:nick message) ")")))))
 
 (defmethod handle-who "is" [irc message _ rest]
   (def conn (db/get-conn irc))
