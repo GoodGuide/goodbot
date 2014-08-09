69a8a0893374bee30c7f2294cc0aed5993e68311
http://jneen.net/ <jneen@jneen.net>
2014-08-09 00:52:40 -0700

    license as EPL

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject goodbot "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "MIT License"
-            :url "http://opensource.org/licenses/MIT"}
+  :url "https://github.com/GoodGuide/goodbot"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [irclj "0.5.0-alpha4"]
                  [com.datomic/datomic-free "0.9.4707"]
