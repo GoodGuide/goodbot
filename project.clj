7da92865480146c97723ac3824f79c3fed2a3cd5
http://jneen.net/ <jneen@jneen.net>
2014-08-08 22:48:43 -0700

    upgrade irclj for less crappy exception handling

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [irclj "0.5.0-alpha3"]
+                 [irclj "0.5.0-alpha4"]
                  [com.datomic/datomic-free "0.9.4707"]
                  [org.clojure/algo.generic "0.1.0"]
                  [clojail "1.0.6"]]
