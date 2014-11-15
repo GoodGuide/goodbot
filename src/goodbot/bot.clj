daf4aaaf01842cef57cee8ab243ce1d86b085506
davidhampgonsalves <davidhampgonsalves@gmail.com>
2014-11-14 20:00:17 -0800

    Plugins can now define polling tasks that are excuted in their own thread at a configured interval. Health plugin was created as an example.

--- src/goodbot/bot.clj
+++ src/goodbot/bot.clj
@@ -4,7 +4,8 @@
    [irclj.core :as irclj]
    [irclj.events]
    [goodbot.db :as db]
-   [goodbot.parse :refer [extract-command]]])
+   [goodbot.parse :refer [extract-command]]
+   [overtone.at-at :as at]])
 
 (defn select-handler [plugins name]
   (->> plugins (map #(get-in % [:commands name])) (remove nil?) first))
@@ -31,6 +32,18 @@
         (println (.getMessage e))
         (.printStackTrace e)))))
 
+(defn schedule-tasks [bot, plugins]
+  "Schedule all plugin tasks"
+  (def task-scheduler-pool (at/mk-pool))
+  (doseq [plugin plugins]
+    (doseq [task (get plugin :tasks)]
+      (at/every
+        (get task :interval)
+        (fn [] ((get task :work) bot))
+        task-scheduler-pool
+        :fixed-delay true
+        :initial-delay 20000))))
+
 (defn start [plugins & {:keys [host port nick password
                                channels server-password
                                datomic-uri]}]
@@ -48,4 +61,5 @@
     (println "connecting to datomic at" datomic-uri)
     (db/start bot))
   (when password (irclj/identify bot password))
-  (doseq [c channels] (println "joining" c) (irclj/join bot c)))
+  (doseq [c channels] (println "joining" c) (irclj/join bot c))
+  (schedule-tasks bot plugins))
